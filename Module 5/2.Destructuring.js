6a540964e371bf28952820be785356c06563c201
Sangeeta Sharma <sangeeta136428sharma@gmail.com>
2024-05-11 09:30:56 +0530

    destructuring

---
+++ Module 5/2.Destructuring.js
@@ -0,0 +1,11 @@
+let pro=
+{
+    name:'iphone 15',
+    price:100000,
+    brand:'Apple',
+    ROM:undefined,
+    ram:'6gb',
+    rom:'128 gb',
+};
+const {brand,ram:Ram,ROM='45gb'}=pro
+console.log(brand,Ram,ROM)
