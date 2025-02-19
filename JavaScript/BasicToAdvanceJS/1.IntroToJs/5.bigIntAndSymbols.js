2c0f54083afbf2ffa685fbb218150b5a9fa2c223
Sangeeta <sangeeta136428sharma@gmail.com>
2025-02-19 22:58:53 +0530

    day6 done

--- JavaScript/BasicToAdvanceJS/1.IntroToJs/5.bigIntAndSymbols.js
+++ JavaScript/BasicToAdvanceJS/1.IntroToJs/5.bigIntAndSymbols.js
@@ -11,7 +11,7 @@ console.log(typeof aVeryBigNumber)
 // `BigInt` type was recently added to the language to represent integers of arbitrary length. A `BigInt` value is created by appending `n` to the end of an integer:
 
 // the "n" at the end means it's a BigInt
-const aVeryBigNumber = 1234567890123456789012345678901234567890n;
+// const aVeryBigNumber = 1234567890123456789012345678901234567890n;
 console.log(typeof aVeryBigNumber)
 
 
