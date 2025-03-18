c4c24c447f71c5942d0bdff84cc4cd00c2308208
Sangeeta <sangeeta136428sharma@gmail.com>
2025-03-18 23:13:43 +0530

    js learn

--- JavaScript/BasicToAdvanceJS/Day14/1.this.js
+++ JavaScript/BasicToAdvanceJS/Day14/1.this.js
@@ -1,10 +1,62 @@
-function sayGoodbye() {
-  console.log("Good bye! ", this);
-}
+// //1st question
+// function sayGoodbye() {
+//   console.log("Good bye! ", this);
+// }
 
+// function sayHello() {
+//   console.log("Helloo! ", this);
+//   sayGoodbye();
+// }
+
+// sayHello();
+
+
+//2. Question
+// function sayHello() {
+//   console.log("Hello! from ", this.name);
+// }
+
+// var john = {
+//   name: "John Doe",
+//   age: 30,
+//   sayName: function () {
+//     // this.age = 35;
+//     console.log("My name is ", this.name);
+//   },
+//   sayHello: sayHello,
+// };
+// // console.log(john.sayHello());
+// // console.log(john.sayName())
+// var james = {
+//   name: "James Bond",
+//   age: 27,
+//   sayName: function () {
+//     console.log("My name is ", this.name);
+//   },
+//   sayHello: sayHello,
+// };
+
+// john.sayName();
+// john.sayHello();
+
+// james.sayName();
+// james.sayHello();
+
+
+//3. question
 function sayHello() {
-  console.log("Helloo! ", this);
-  sayGoodbye();
+  console.log("Hello! ", this);
 }
 
-sayHello();
+var john = {
+  name: "John Doe",
+  age: 30,
+};
+
+var james = {
+  name: "James Bond",
+  age: 30,
+};
+
+sayHello.call(james);
+sayHello.call(john);
